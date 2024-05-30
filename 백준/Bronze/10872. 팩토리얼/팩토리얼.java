48db6127151cab1f3b9d17de47121a88b0eec3a4
113mj <128031749+113mj@users.noreply.github.com>
2024-05-30 22:14:29 +0900

    [Bronze III] Title: 팩토리얼, Time: 208 ms, Memory: 17608 KB -BaekjoonHub

--- 백준/Bronze/10872. 팩토리얼/팩토리얼.java
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.java
@@ -1,16 +1,23 @@
 import java.util.Scanner;
 
+
+
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int a = scanner.nextInt();
         int answer = 1;
 
-        for (int i = 1; i <= a; i++) {
-            answer *= i;
+        for(int i = 1; i<=a; i++)
+        {
+            if (i==0)
+                answer = 1;
+            else
+                answer = answer * i;
         }
 
         System.out.println(answer);
-        scanner.close();
     }
 }
+
+
