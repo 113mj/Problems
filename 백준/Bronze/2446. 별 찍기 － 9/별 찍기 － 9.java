1b4bfc2f8fb7d995c66cecea7e3a1b872f11cad6
113mj <128031749+113mj@users.noreply.github.com>
2024-06-23 13:40:21 +0900

    [Bronze III] Title: 별 찍기 - 9, Time: 564 ms, Memory: 22696 KB -BaekjoonHub

---
+++ 백준/Bronze/2446. 별 찍기 － 9/별 찍기 － 9.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        f(a);
+    }
+
+    public static void f(int x) {
+        // 역 피라미드 출력
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k < (2 * x - 1) - 2 * i; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        // 피라미드 출력
+        for (int i = 2; i <= x; i++) {
+            for (int j = x; j > i; j--) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k < (2 * i) - 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
