a16ce220b0d58f0d3374210fd217fcb7f876196f
113mj <128031749+113mj@users.noreply.github.com>
2024-08-12 21:08:29 +0900

    [Bronze II] Title: 텔레프라임, Time: 152 ms, Memory: 17472 KB -BaekjoonHub

---
+++ 백준/Bronze/7789. 텔레프라임/텔레프라임.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken() + String.valueOf(A));
+        
+        int i = 2;
+        String result = "Yes";
+        
+        while (i < B) {
+            if (i < A && A % i == 0) {
+                result = "No";
+                break;
+            }
+            if (i < B && B % i == 0) {
+                result = "No";
+                break;
+            }
+            i++;
+        }
+        System.out.println(result);
+    }
+}
