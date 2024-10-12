39c8baa893f3d6bdf84055e1ef79977d11840947
113mj <128031749+113mj@users.noreply.github.com>
2024-10-12 23:53:30 +0900

    [Bronze V] Title: 콜센터, Time: 96 ms, Memory: 13868 KB -BaekjoonHub

---
+++ 백준/Bronze/5339. 콜센터/콜센터.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.out.println("     /~\\");
+        System.out.print("    ( oo|\n");
+        System.out.print("    _\\=/_\n");
+        System.out.print("   /  _  \\\n");
+        System.out.print("  //|/.\\|\\\\\n");
+        System.out.print(" ||  \\ /  ||\n");
+        System.out.print("============\n");
+        System.out.print("|          |\n");
+        System.out.print("|          |\n");
+        System.out.print("|          |\n");
+
+    }
+}
