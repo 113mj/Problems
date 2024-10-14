7257a4ca5bb03ad5e6d8b73708eb2dd608ee693c
113mj <128031749+113mj@users.noreply.github.com>
2024-10-14 21:55:24 +0900

    [Bronze V] Title: 코드마스터 2023, Time: 96 ms, Memory: 14192 KB -BaekjoonHub

---
+++ 백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.java
@@ -0,0 +1,19 @@
+import java.io.*;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+
+        if (str.equals("SONGDO"))
+            System.out.println("HIGHSCHOOL");
+        else if (str.equals("CODE"))
+            System.out.println("MASTER");
+        else if (str.equals("2023"))
+            System.out.println("0611");
+        else if (str.equals("ALGORITHM"))
+            System.out.println("CONTEST");
+
+    }
+}
