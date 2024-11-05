340e2993706334d2239d46122610022e37b6092c
113mj <128031749+113mj@users.noreply.github.com>
2024-11-05 23:15:58 +0900

    [Bronze I] Title: 약수, Time: 108 ms, Memory: 14320 KB -BaekjoonHub

---
+++ 백준/Bronze/1037. 약수/약수.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+
+        while(T-- > 0) {
+            int N = Integer.parseInt(st.nextToken());
+            max = Math.max(N, max);
+            min = Math.min(N, min);
+        }
+        System.out.println(max * min);
+    }
+}
