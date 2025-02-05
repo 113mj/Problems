b1b9d236bef475a3962bb5142f07ad0851726c32
113mj <128031749+113mj@users.noreply.github.com>
2025-02-05 17:40:02 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.py
@@ -0,0 +1,7 @@
+def solution(numbers):
+    numbers.sort()
+    if (numbers[0] * numbers[1] > numbers[len(numbers)-1] * numbers[len(numbers)-2]):
+        answer = numbers[0] * numbers[1]
+    else:
+        answer = numbers[len(numbers)-1] * numbers[len(numbers)-2]
+    return answer
